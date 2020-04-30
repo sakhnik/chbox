77f3cdfb06d50b6a91f380091a9d2db406a04557
Anatolii Sakhnik <sakhnik@gmail.com>
2020-04-30 11:41:55 +0300

    Remove support for nspawn Arch

--- bin/run-in-archbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cmd=$(basename "${BASH_SOURCE[0]}")
-
-if [[ "$(systemd-detect-virt)" == none ]]; then
-    sudo machinectl shell sakhnik@arch /usr/bin/$cmd
-else
-    /usr/bin/$cmd
-fi
