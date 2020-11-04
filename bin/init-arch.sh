65f4d71e1c98c89d51f19ccdcb9a1d5b93e33f17
Anatolii Sakhnik <sakhnik@gmail.com>
2020-11-04 20:15:33 +0200

    Use machinectl shell to connect to the container

--- bin/init-arch.sh
+++ bin/init-arch.sh
@@ -7,4 +7,4 @@ sleep 1
 sudo machinectl shell root@arch /usr/bin/mkdir -p /run/user/host/pulse
 sudo machinectl bind arch /run/user/$UID/pulse /run/user/host/pulse
 
-sudo machinectl login arch
+sudo machinectl shell $USER@arch
