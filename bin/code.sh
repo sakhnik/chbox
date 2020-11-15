478ea8b171452e18838c81b986bb4d00353c804f
Anatolii Sakhnik <sakhnik@gmail.com>
2020-11-15 14:34:13 +0200

    Update vscode

--- bin/code.sh
+++ bin/code.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 url="https://update.code.visualstudio.com/latest/linux-x64/stable"
+#url="https://update.code.visualstudio.com/latest/linux-x64/insider"
 fname="code.tar.gz"
 
 cd /tmp
@@ -12,5 +13,8 @@ fi
 if [[ ! -d VSCode-linux-x64 ]]; then
     tar -xvf "$fname"
 fi
+
+mkdir -p /tmp/vscode-cpptools
+ln -sf /tmp/vscode-cpptools ~/.cache/
 
 ./VSCode-linux-x64/code "$@"
