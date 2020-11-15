5471cae65669563c3b5fd4520efce606ef77371d
Anatolii Sakhnik <sakhnik@gmail.com>
2020-11-15 14:33:34 +0200

    Change yt.sh to use VLC for compatibility with RPI4

--- bin/yt.sh
+++ bin/yt.sh
@@ -1,14 +1,28 @@
-#!/bin/bash -ex
+#!/bin/bash -x
+
+runVlc()
+{
+    urls="$1"
+    if [[ $(echo "$urls" | wc -l) -gt 1 ]]; then
+        vurl="$(echo "$urls" | head -1)"
+        aurl="$(echo "$urls" | head -2 | tail -1)"
+        vlc "$vurl" --input-slave "$aurl"
+    else
+        vlc "$urls"
+    fi
+}
+
+fetchUrls()
+{
+    for i in 1 2 3; do
+        urls="$(youtube-dl --format="bestvideo[height<=?1080]+bestaudio/best" -g "$1")"
+        if [[ $? -eq 0 ]]; then
+            runVlc "$urls"
+            break
+        fi
+    done
+}
 
 while read url; do
-    mpv "$url"
+    fetchUrls "$url"
 done
-
-#urls="$(youtube-dl --format="bestvideo[height<=?1080]+bestaudio/best" -g "$1")"
-#if [[ $(echo "$urls" | wc -l) -gt 1 ]]; then
-#    vurl="$(echo "$urls" | head -1)"
-#    aurl="$(echo "$urls" | head -2 | tail -1)"
-#    vlc "$vurl" --input-slave "$aurl"
-#else
-#    vlc "$urls"
-#fi
