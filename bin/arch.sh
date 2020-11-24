81dab94a87bcb0969e358aacb050d7bafa2ac84f
Anatolii Sakhnik <sakhnik@gmail.com>
2020-11-24 10:27:47 +0200

    Adapt to raspios's paths

--- bin/arch.sh
+++ bin/arch.sh
@@ -5,9 +5,9 @@ pserver=/tmp/pulse-host/$USER
 machine=$(basename ${BASH_SOURCE[0]})
 machine=${machine%.sh}
 
-if ! (machinectl -q shell $USER@$machine /usr/bin/mount | grep -q $pserver); then
+if ! (machinectl -q shell $USER@$machine /bin/mount | grep -q $pserver); then
     # Allow Arch accessing pulseaudio
-    machinectl -q shell $USER@$machine /usr/bin/mkdir -p $pserver
+    machinectl -q shell $USER@$machine /bin/mkdir -p $pserver
     machinectl bind --read-only $machine /run/user/$UID/pulse $pserver
 fi
 
