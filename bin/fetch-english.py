87f21bcb6f071263ff69e3ed688f731ae13fe15c
Anatolii Sakhnik <sakhnik@gmail.com>
2022-07-28 19:04:06 +0300

    Use gtk4 to fetch and copy pronunciations

--- bin/fetch-english.py
+++ bin/fetch-english.py
@@ -1,27 +1,78 @@
 #!/usr/bin/env python
 
-import sys
+# Fetch English transcription and pronunciation for an entered word.
+# Copy them to the clipboard by clicking on the button.
+
 import requests
 import random
 from bs4 import BeautifulSoup
+import gi
+gi.require_version("Gtk", "4.0")
+from gi.repository import Gtk
 
-word = sys.argv[1]
-url = f"https://www.oxfordlearnersdictionaries.com/search/english/direct/?q={word}"
 
 user_agents = [
   "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
   "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
   "Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
   ]
-random_user_agent = random.choice(user_agents)
-headers = {
-    'User-Agent': random_user_agent
-}
 
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
-for phon_br in soup.find_all('div', 'phons_br'):
-    for t in phon_br.find_all('span', 'phon'):
-        print(t.text)
-    for t in phon_br.find_all('div', 'pron-uk'):
-        print(t.get('data-src-ogg', t.get('data-src-mp3')))
+
+def Fetch(word):
+    url = f"https://www.oxfordlearnersdictionaries.com/search/english/direct/?q={word}"
+
+    random_user_agent = random.choice(user_agents)
+    headers = {
+        'User-Agent': random_user_agent
+    }
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    defs = []
+    for phon_br in soup.find_all('div', 'phons_br'):
+        row = []
+        for t in phon_br.find_all('span', 'phon'):
+            row.append(t.text)
+        for t in phon_br.find_all('div', 'pron-uk'):
+            row.append(t.get('data-src-ogg', t.get('data-src-mp3')))
+        defs.append(row)
+    return defs
+
+
+def FillTheGrid(grid, word):
+    while True:
+        if grid.get_child_at(0, 1) is not None:
+            grid.remove_row(1)
+        else:
+            break
+
+    defs = Fetch(word)
+    for r, row in enumerate(defs):
+        for c, d in enumerate(row):
+            btn = Gtk.Button(label=d)
+            grid.attach(btn, c, r, 1, 1)
+            btn.connect('clicked',
+                        lambda x: x.get_clipboard().set(x.get_label()))
+
+
+def on_activate(app):
+    win = Gtk.ApplicationWindow(application=app)
+
+    entry = Gtk.Entry()
+    entry.set_placeholder_text("Type and English word and press Enter")
+
+    grid = Gtk.Grid()
+
+    box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+    box.append(entry)
+    box.append(grid)
+
+    entry.connect('activate', lambda x: FillTheGrid(grid, x.get_text()))
+
+    win.set_child(box)
+    win.present()
+
+
+app = Gtk.Application(application_id='org.gtk.Example')
+app.connect('activate', on_activate)
+app.run(None)
