8abd4812ced4a307b3ebe5381e9bb96f566db2fa
Anatolii Sakhnik <sakhnik@gmail.com>
2022-07-28 17:23:58 +0300

    Fix LosslessCut

--- bin/LosslessCut.sh
+++ bin/LosslessCut.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 cd /tmp
-wget -c 'https://github.com/mifi/lossless-cut/releases/latest/download/LosslessCut-linux.AppImage'
-chmod +x ./LosslessCut-linux.AppImage
-./LosslessCut-linux.AppImage
+wget -c 'https://github.com/mifi/lossless-cut/releases/latest/download/LosslessCut-linux-x86_64.AppImage'
+chmod +x ./LosslessCut-linux-x86_64.AppImage
+./LosslessCut-linux-x86_64.AppImage
